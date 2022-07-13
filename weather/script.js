dbe0726aec4168f8d3cdc939b7e441788e70896e
Muhammad Waqar <MW6069605@GMAIL.COM>
2022-07-13 20:12:04 +0500

    Add files via upload

---
+++ weather/script.js
@@ -0,0 +1,25 @@
+function getWeather(city){
+	if (city) {
+	var xhr=new XMLHttpRequest();
+	xhr.onreadystatechange=function(){
+		if (this.status==200 && this.readyState==4) {
+			var formattedData=formatWeather(JSON.parse(xhr.responseText));
+			document.getElementById("weather-data").innerHTML=formattedData;
+			document.getElementById('cityname').value="";
+		}
+	};
+	xhr.open("GET","http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=18b4f330fe04002b202faf937b3e5d1a");
+	xhr.send();
+  }
+}
+function formatWeather(data){
+	return "<h3>Current Weather for " + data.name + ", " + data.sys.country + "</h3>" + 
+			"<p>Weather: " + data.weather[0].main+ "</p>" + 
+			"<p>Weather Description: " + data.weather[0].description +"<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'/>" + "</p>" + 
+			"<p>Temperature: " + data.main.temp + "&deg;C</p>" + 
+			"<p>Pressure: " + data.main.pressure + "hPa</p>" + 
+			"<p>Humidity: " + data.main.humidity + "%</p>" + 
+			"<p>Min Temperature: " + data.main.temp_min + "&deg;C</p>" + 
+			"<p>Max Temperature: " + data.main.temp_max + "&deg;C</p>" + 
+			"<p>Wind Speed: " + data.wind.speed + "m/s</p>";
+}
